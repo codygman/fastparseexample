25131fb9c3b27ae1592d1f4dfd93bb9dfc5875a6
Cody Goodman <codygman.consulting@gmail.com>
2017-07-24 22:29:13 -0500

    working

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,49 @@
 package example
 
+object Attempt1 {
+  import fastparse._
+
+  val White = WhitespaceApi.Wrapper{
+    import fastparse.all._
+    NoTrace( (" " | "\r\n").rep )
+  }
+
+  import fastparse.noApi._
+  import White._
+
+  sealed trait Ty
+  case class AInt(name: String, length: Int)
+  case class AString(name: String, length: Int)
+  case class Grammar(rows : Seq[Ty])
+
+  val identifier = P (CharIn('A' to 'Z'))
+  val digit = P( CharIn('0' to '9').rep(1) ).!.map(_.toInt)
+  val intRow: P[AInt] = P( "INT(" ~ digit ~ ")" ~ identifier.!).map{case (num,ident) => AInt(ident, num)}
+  val stringRow: P[AString] = P( "STRING(" ~ digit ~ ")" ~ identifier.!).map{case (num,ident) => AString(ident,num)}
+  val row = intRow | stringRow
+  val grammar = P("START" ~ row.rep(sep=";") ~ ";".rep(1) ~ "END".rep(1))
+}
+
+object Hello extends App {
+  val fullEx = """START
+INT(3) X;
+STRING(3) X;
+END
+"""
+  println(Attempt1.row.parse("INT(3) X"))
+  println(Attempt1.row.parse("STRING(3) X"))
+  println(Attempt1.grammar.parse(fullEx))
+}
+
+
+/*
+I have a fastparse parser 'P( "INT(" ~ digit ~ ")" ~ identifier).map{case num => AInt("",num)}', I expected both the capture of digit and identifier to be mapped to where num is, but it seems only num is captured. How can I capture both?
+
+
+ I have a parser 'P( "INT(" ~ digit ~ ")" ~ identifier).map{case num => AInt("",num)}', I expected both the capture of digit and identifier to be mapped to where num is, but it seems only num is captured. How can I capture both?
+ 
+ */
+
 /*
  Grammar:
 
@@ -31,51 +75,12 @@ package example
  val stringRow: P[Int] = P( "STRING(" ~ digit ~ ")" )
  */
 
-object Foo {
-  import fastparse.all._
-  val plus = P( "+" )
-  val num = P( CharIn('0' to '9').rep(1) ).!.map(_.toInt)
-  val side = P( "(" ~ expr ~ ")"| num )
-  val expr : P[Int] = P(side ~ plus ~ side).map{case (l,r) => l + r}
-}
-
-object Attempt1 {
-  import fastparse.all._
-
-  sealed trait Ty
-  case class AInt(name: String, length: Int)
-  case class AString(name: String, length: Int)
-  case class Grammar(rows : Seq[Ty])
-
-  val identifier = P (CharIn('A' to 'Z'))
-  val digit = P( CharIn('0' to '9').rep(1) ).!.map(_.toInt)
-  val intRow: P[AInt] = P( "INT(" ~ digit ~ ") " ~ identifier.!).map{case (num,ident) => AInt(ident, num)}
-  val stringRow: P[AString] = P( "STRING(" ~ digit ~ ") " ~ identifier.!).map{case (num,ident) => AString(ident,num)}
-  val row = intRow | stringRow
-  val grammar = P("START\r\n" ~ row.rep(sep="\r\n") ~ "\r\nEND\r\n")
-}
-
-object Hello extends Greeting with App {
-  // println(greeting)
-  // println(Foo.expr.parse("123+123"))
-  val fullEx = """START
-INT(3) X
-STRING(3) X
-END
-"""
-  println(Attempt1.row.parse("INT(3) X"))
-  println(Attempt1.row.parse("STRING(3) X"))
-  println(Attempt1.grammar.parse(fullEx))
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
-}
-
-/*
-I have a fastparse parser 'P( "INT(" ~ digit ~ ")" ~ identifier).map{case num => AInt("",num)}', I expected both the capture of digit and identifier to be mapped to where num is, but it seems only num is captured. How can I capture both?
+// object Foo {
+//   import fastparse.all._
+//   val plus = P( "+" )
+//   val num = P( CharIn('0' to '9').rep(1) ).!.map(_.toInt)
+//   val side = P( "(" ~ expr ~ ")"| num )
+//   val expr : P[Int] = P(side ~ plus ~ side).map{case (l,r) => l + r}
+// }
 
 
- I have a parser 'P( "INT(" ~ digit ~ ")" ~ identifier).map{case num => AInt("",num)}', I expected both the capture of digit and identifier to be mapped to where num is, but it seems only num is captured. How can I capture both?
- 
- */
